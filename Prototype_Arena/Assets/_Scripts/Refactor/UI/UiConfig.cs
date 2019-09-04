00b9cd90ca94bbe48a78e3f0e251f1960bac0142
Steven Dijcks <steven.dijcks@kolibrigames.com>
2019-09-04 23:25:49 +0200

    Adds ui config for ui prefabs

--- Prototype_Arena/Assets/_Scripts/Refactor/UI/UiConfig.cs
+++ Prototype_Arena/Assets/_Scripts/Refactor/UI/UiConfig.cs
@@ -1,7 +1,51 @@
+using UnityEngine;
+
 namespace _Scripts.Refactor.UI
 {
-    public class UiConfig
+    [CreateAssetMenu(menuName = "ScriptableObject/UI/UiConfig", fileName = "UiConfig")]
+    public class UiConfig : ScriptableObject
     {
+        [SerializeField] private MainUiView _mainUiPrefab;
+        [SerializeField] private ResolveUiView _resolveUiPrefab;
+        [SerializeField] private EndScreenUiView _endScreenUiPrefab;
+        [SerializeField] private GridUiView _gridUiPrefab;
+        [SerializeField] private MenuUiView _menuUiPrefab;
+        [SerializeField] private DraftUiView _draftUiPrefab;
+        [SerializeField] private PlanUiView _planUiPrefab;
+
+        public MainUiView MainUiPrefab
+        {
+            get { return _mainUiPrefab; }
+        }
         
+        public ResolveUiView ResolveUiPrefab
+        {
+            get { return _resolveUiPrefab; }
+        }
+
+        public EndScreenUiView EndScreenUiPrefab
+        {
+            get { return _endScreenUiPrefab; }
+        }
+
+        public GridUiView GridUiPrefab
+        {
+            get { return _gridUiPrefab; }
+        }
+
+        public MenuUiView MenuUiPrefab
+        {
+            get { return _menuUiPrefab; }
+        }
+        
+        public DraftUiView DraftUiPrefab
+        {
+            get { return _draftUiPrefab; }
+        }
+        
+        public PlanUiView PlanUiPrefab
+        {
+            get { return _planUiPrefab; }
+        }
     }
 }
